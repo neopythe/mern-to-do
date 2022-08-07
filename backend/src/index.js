79880159a0da5132e509894dc6a64338a31ec7ff
Evan Bay <neopythe@protonmail.ch>
2022-08-06 20:54:47 -0600

    establish database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv') // load environment variables from .env file
 const cors = require('cors') // enable cross-origin resource sharing
 const morgan = require('morgan') // log requests - improved debugging logic
+const mongoose = require('mongoose')
 
 const router = require('./router')
 
@@ -14,6 +15,8 @@ app.use(morgan('dev'))
 app.use(router)
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`Serving on port ${port}`)
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+  })
 })
