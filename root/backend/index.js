f12ad9e8136537fcd142b2221f7998833e24c895
Evan Bay <neopythe@protonmail.ch>
2022-08-06 18:45:34 -0600

    wire up express server

---
+++ root/backend/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`)
+})
