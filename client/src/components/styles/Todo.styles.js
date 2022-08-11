105746be2c157dbec472f10700bced8af9e99ab8
Evan Bay <neopythe@protonmail.ch>
2022-08-10 22:46:54 -0600

    create IconContainer styled component for Todo

--- client/src/components/styles/Todo.styles.js
+++ client/src/components/styles/Todo.styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  height: 100%;
+`
+
 export const StyledTodo = styled.li`
   display: flex;
   justify-content: space-between;
