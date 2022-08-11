99350f58b1eabe81fb3cb04362589d21c25e0944
Evan Bay <neopythe@protonmail.ch>
2022-08-11 06:58:59 -0600

    fix padding issue on TodoList

--- client/src/components/styles/TodoList.styles.js
+++ client/src/components/styles/TodoList.styles.js
@@ -1,22 +1,23 @@
 import styled from 'styled-components'
 
-export const StyledTodoList = styled.section`
+export const StyledTodoList = styled.main`
   display: flex;
   justify-content: center;
   margin: 1rem 0;
-  padding: 0.5rem;
-  background-color: #2b2a33;
+  background-color: #fff;
   max-height: 80vh;
-  overflow-y: scroll;
-  scrollbar-width: thin;
-  scrollbar-color: #696969 #1a1a1a;
+  box-shadow: 0px 25px 50px -12px rgba(0, 0, 0, 0.25);
+  padding: 0;
 `
 
 export const StyledUnorderedList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
+  padding: 2rem;
   margin: 0;
-  padding: 0;
   width: 100%;
+  overflow-y: scroll;
+  scrollbar-width: thin;
+  scrollbar-color: #696969 #1a1a1a;
 `
