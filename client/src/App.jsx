f4211354ffbd8a45b3ac8709f2a36170496fc2d2
Evan Bay <neopythe@protonmail.ch>
2022-08-11 06:32:20 -0600

    create AppContainer styled component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,37 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { TodoProvider } from './context/TodoContext'
 
 import NewTodoForm from './components/NewTodoForm'
 import TodoList from './components/TodoList'
 
+const AppContainer = styled.div`
+  max-width: 600px;
+  width: 100%;
+  max-height: 100%;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+  overflow: hidden;
+
+  @media only screen and (min-width: 600px) {
+    & {
+      width: 80%;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    & {
+      width: 60%;
+    }
+  }
+`
+
 export default function App() {
   return (
     <TodoProvider>
-      <>
+      <AppContainer>
         <Routes>
           <Route exact path="/" element={<Navigate to="/todos" />} />
           <Route
@@ -22,7 +45,7 @@ export default function App() {
             }
           />
         </Routes>
-      </>
+      </AppContainer>
     </TodoProvider>
   )
 }
