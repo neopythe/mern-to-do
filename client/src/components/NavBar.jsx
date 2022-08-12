41c7b453e5230ee382e3ff1cf4747a6008f68656
Evan Bay <neopythe@protonmail.ch>
2022-08-12 13:05:31 -0600

    add app logo to NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,10 @@ import Button from './shared/Button'
 
 export default function NavBar() {
   return (
-    <nav className="navbar justify-end absolute bg-white text-slate-800 w-full">
+    <nav className="navbar justify-between absolute bg-white text-slate-800 w-full">
+      <h2 className="font-bold text-xl tracking-wide underline decoration-[#626977] underline-offset-4 decoration-wavy">
+        MERN to-do
+      </h2>
       <Button navigation>logout</Button>
     </nav>
   )
