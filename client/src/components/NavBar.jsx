3f95b16fbd9203463ae55566f93470a064edffe6
Evan Bay <neopythe@protonmail.ch>
2022-08-13 00:39:45 -0600

    hide logout button

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
       <h2 className="font-bold text-xl tracking-wide underline decoration-[#626977] underline-offset-4 decoration-wavy">
         MERN to-do
       </h2>
-      <Button navigation>logout</Button>
+      {/* <Button navigation>logout</Button> */}
     </nav>
   )
 }
